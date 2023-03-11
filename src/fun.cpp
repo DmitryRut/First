38611135bf53de2009407895d58c7708d5a1a08a
DmitryRut <dmalrut@mail.ru>
2023-03-11 22:35:05 +0300

    first commit

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,71 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <ctype.h>
+#include <cstring>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str) {
+	int count = 0;
+	bool isWord = false, space = true;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isspace(str[i]))
+		{
+			space = true;
+			if (isWord) count++;
+			isWord = false;
+		}
+		else
+		{
+			if ((space && !isdigit(str[i])) || (isWord && !isdigit(str[i])))
+				isWord = true;
+			if (isdigit(str[i])) isWord = false;
+			space = false;
+		}
+	}
+	if (isWord) count++;
+	return count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+	int count = 0;
+	bool isWord = false, space = true;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isspace(str[i]))
+		{
+			space = true;
+			if (isWord) count++;
+			isWord = false;
+		}
+		else
+		{
+			if (space && isupper(str[i]) && !isdigit(str[i])) isWord = true;
+			else
+				if (isWord && isupper(str[i]) || isWord && isdigit(str[i])) isWord = false;
+			space = false;
+		}
+	}
+	if (isWord) count++;
+	return count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+	int countWord = 0, count = 0;
+	bool isWord = false, space = true;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isspace(str[i]))
+		{
+			space = true;
+			if (isWord) countWord++;
+			isWord = false;
+		}
+		else
+		{
+			count++;
+			isWord = true;
+		}
+	}
+	return (count + countWord / 2) / countWord;
+	//return round((float)(count / countWord));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
-    return 0;
+	char str[] = { " M 4R Kq4 vbim " };
+	int n1, n2, n3;
+	n1 = faStr1(str);
+	n2 = faStr2(str);
+	n3 = faStr3(str);
+	printf("%d\n", n1);
+	printf("%d\n", n2);
+	printf("%d", n3);
+	return 0;
 }
